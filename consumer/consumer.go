dcded93fe051c3e1da523deecac7e644accb16ba
JavierAlfaroGT <javieralfarogt@gmail.com>
2022-10-12 17:43:00 -0600

    app 2.0

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -1,4 +1,5 @@
-//lee todo el batch
+//de una vez jala todo el batch
+
 package main
 
 import (
@@ -21,13 +22,10 @@ func main() {
 	//configuracion de tiempo
 	conn.SetWriteDeadline(time.Now().Add(time.Second * 8))
 
-	//configuracion de tiempo
-	conn.SetWriteDeadline(time.Now().Add(time.Second * 8))
-
 	//configuracion de lectura de mensajes ingresados a kafka
 	batch := conn.ReadBatch(1e3, 1e6) //1e3=1000
 
-	// bytes := make([]byte, 1e6) //ojo con el tam 1e9 no lo soporta xd
+	//       bytes := make([]byte, 1e6) //ojo con el tam 1e9 no lo soporta xd
 
 	for {
 		bytes := make([]byte, 1e3) //ojo con el tam 1e9 no lo soporta xd
--- producer/producer.go
+++ producer/producer.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"time"
 
 	"github.com/segmentio/kafka-go"
 )
+
+type Obj struct {
+	Id   string          `json:"id"`
+	Data json.RawMessage `json:"data"`
+}
 
 func main() {
 
@@ -21,6 +27,25 @@ func main() {
 	//configuracion de tiempo
 	conn.SetWriteDeadline(time.Now().Add(time.Second * 10))
 
+	//-------------------------------- de []bytes a JSON--------------------
+	byt := []byte(`{"id":"someID","data":"hello world"}`)
+
+	var obj Obj
+	if err := json.Unmarshal(byt, &obj); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(obj)
+
+	//---------------------------------de JSON a []bytes
+	b, _ := json.Marshal(obj.Data)
+	// Convert bytes to string.
+	s := string(b)
+	fmt.Println(s)
+	//----------------------------------------------------------------------
+
 	//configuracion del mensaje a ingresar a kafka
-	conn.WriteMessages(kafka.Message{Value: []byte("hello world from golang")})
+	conn.WriteMessages(kafka.Message{Value: b})
+	//	conn.WriteMessages(kafka.Message{Value: []byte("hello world from golang")})
+
 }
